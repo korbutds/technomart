7b919a9fc2477261752b905c9295257124e9059e
Korbutds <sys@hohou.ru>
2020-02-27 08:20:21 +0300

    21. Личный проект: оживляем интерактивные элементы

--- js/script.js
+++ js/script.js
@@ -56,3 +56,29 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
+var mapButton = document.querySelector('.map-section button');
+var mapPopUp = document.querySelector('.map-pop-up');
+var mapClose = mapPopUp.querySelector('.close-btn');
+
+mapButton.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  mapPopUp.classList.add('pop-up-close');
+  mapPopUp.classList.remove('pop-up-close');
+})
+
+mapClose.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  mapPopUp.classList.add("pop-up-close");
+})
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (mapPopUp.classList.contains("pop-up-close")) {
+
+    } else {
+      mapPopUp.classList.add("pop-up-close");
+    }
+  }
+});
