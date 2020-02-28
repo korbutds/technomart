790014907d3402dce922ee18c07cd0df40c254e5
Korbutds <sys@hohou.ru>
2020-02-28 16:14:33 +0300

    Личный проект: добавляем интерактивную карту

--- js/script.js
+++ js/script.js
@@ -15,11 +15,11 @@ var storageEmail = localStorage.getItem("");
 
 var mapButton = document.querySelector(".map-section button");
 var mapPopUp = document.querySelector(".map-pop-up");
-var mapClose = document.querySelector(".close-btn");
+var mapClose = document.querySelector(".map-pop-up .close-btn");
 
 var basketButton = document.querySelectorAll(".basket-btn");
 var cartPopUp = document.querySelector(".cart-section");
-var cartClose = cartPopUp.querySelector(".close-btn");
+var cartClose = document.querySelector(".cart-section .close-btn");
 
 try {
   storageName = localStorage.getItem("userName");
@@ -28,53 +28,54 @@ try {
   isStorageSupport = false;
 }
 
-writeBtn.addEventListener("click", function(evt) {
+if (writePopUp) {
+  writeBtn.addEventListener("click", function(evt) {
   evt.preventDefault();
   writePopUp.classList.remove(popupClose);
-  if (storageName) {
-    userName.value = storageName;
-    email.value = storageEmail;
-    letter.focus();
-  } else {
-    userName.focus();
-  }
-})
-
-closeWrite.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  writePopUp.classList.add(popupClose);
-  writePopUp.classList.remove(popupError);
-})
-
-form.addEventListener("submit", function(evt) {
-  if (!userName.value || !email.value || !letter.value) {
-    evt.preventDefault();
-    writePopUp.classList.remove(popupError);
-    writePopUp.offsetWidth = writePopUp.offsetWidth;
-    writePopUp.classList.add(popupError);
-  } else  {
-    if (isStorageSupport) {
-    localStorage.setItem("userName", userName.value);
-    localStorage.setItem("email", email.value);
+    if (storageName) {
+      userName.value = storageName;
+      email.value = storageEmail;
+      letter.focus();
+    } else {
+      userName.focus();
     }
-  }
-})
+  })
+  closeWrite.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    writePopUp.classList.add(popupClose);
+    writePopUp.classList.remove(popupError);
+  })
+  form.addEventListener("submit", function(evt) {
+    if (!userName.value || !email.value || !letter.value) {
+      evt.preventDefault();
+      writePopUp.classList.remove(popupError);
+      writePopUp.offsetWidth = writePopUp.offsetWidth;
+      writePopUp.classList.add(popupError);
+    } else  {
+      if (isStorageSupport) {
+        localStorage.setItem("userName", userName.value);
+        localStorage.setItem("email", email.value);
+      }
+    }
+  })
+}
 
-mapButton.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  mapPopUp.classList.add(popupClose);
-  mapPopUp.classList.remove(popupClose);
-})
+if (mapPopUp) {
+  mapButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    mapPopUp.classList.add(popupClose);
+    mapPopUp.classList.remove(popupClose);
+  })
 
-mapClose.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  mapPopUp.classList.add(popupClose);
-})
+  mapClose.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    mapPopUp.classList.add(popupClose);
+  })
+}
 
 for (var i=0; i < basketButton.length; i++) {
   basketButton[i].addEventListener("click", function(evt) {
     evt.preventDefault();
-    cartPopUp.classList.add(popupClose);
     cartPopUp.classList.remove(popupClose);
   })
 }
@@ -87,14 +88,14 @@ cartClose.addEventListener("click", function(evt) {
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (writePopUp && !writePopUp.classList.contains(popupClose)  || !cartPopUp.classList.contains(popupClose)) {
+    if (writePopUp && !writePopUp.classList.contains(popupClose)) {
       writePopUp.classList.add(popupClose);
       writePopUp.classList.remove(popupError);
     }
     if (mapPopUp && !mapPopUp.classList.contains(popupClose)) {
       mapPopUp.classList.add(popupClose);
     }
-    if (!cartPopUp.classList.contains(popupClose)) {
+    if (cartPopUp && !cartPopUp.classList.contains(popupClose)) {
       cartPopUp.classList.add(popupClose);
     }
   }
